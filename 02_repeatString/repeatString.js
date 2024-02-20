7ac22a2e00735628cad37a70063c8726b2f74d5a
Hectorinho <whatev@supposedEmail.net>
2024-02-20 19:16:35 +0200

    Do 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    rstr = '';
+    for (let i = 0; i < num; i++) rstr += str;
+    return rstr;
 
 };
 
