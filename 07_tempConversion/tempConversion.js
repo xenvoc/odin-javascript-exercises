385e2e82da2e9823e1814b1f26ed93dfbe8c0fc0
Hectorinho <whatev@supposedEmail.net>
2024-02-22 15:58:13 +0200

    Do 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (!Number.isFinite(temp)) return 'ERROR';
+  return +((temp - 32) * 5/9).toFixed(1); 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (!Number.isFinite(temp)) return 'ERROR';
+  return +((temp * 1.8) + 32).toFixed(1);
 };
 
 // Do not edit below this line
