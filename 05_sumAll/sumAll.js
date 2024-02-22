07dac9a8d1eeecb719011798ce0c4af13050ac6e
Hectorinho <whatev@supposedEmail.net>
2024-02-22 14:43:05 +0200

    Do 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') return 'ERROR';
+    if (max < min) {
+    max = max + min;
+    min = max - min;
+    max = max - min;
+    } 
+    rsum = 0;
+    for (let i = min; i <= max; i++) {
+        rsum += i;
+    }
+    return rsum;
 };
 
 // Do not edit below this line
