4518d1c5b0f3b4b7beabf6b91364e5d7b6f0c9df
Hectorinho <whatev@supposedEmail.net>
2024-02-22 13:56:43 +0200

    Do 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arr.indexOf(arguments[i]);
+        while (index > -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(arguments[i]);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
