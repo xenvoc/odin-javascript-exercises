c13cc9b84ed3cdea7b7854a5dfc9e3819a294225
Hectorinho <whatev@supposedEmail.net>
2024-03-12 18:08:14 +0200

    Do 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    else if (index < 1) return 0;
+    else if (index < 3) return 1;
+    else return fibonacci(index-1) + fibonacci(index-2);
 };
 
 // Do not edit below this line
