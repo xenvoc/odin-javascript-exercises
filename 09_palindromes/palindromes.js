c780d39dfaf04fdc17c55d98dccd57fab0fdb0f9
Hectorinho <whatev@supposedEmail.net>
2024-03-12 17:58:52 +0200

    Do 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let plainStr =  str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    for (let i = 0; i < Math.floor(plainStr.length/2); i++) {
+        if (plainStr[i] !== plainStr[plainStr.length - 1 - i]) return false;
+    }
+    return true;
 };
 
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 // Do not edit below this line
 module.exports = palindromes;
