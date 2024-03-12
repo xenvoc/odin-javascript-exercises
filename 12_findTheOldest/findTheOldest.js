6aa17c534df403e35b479186367580933457a7c4
Hectorinho <whatev@supposedEmail.net>
2024-03-12 18:58:26 +0200

    Do 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((currOldest, person) => {
+        const currYear = (new Date()).getUTCFullYear();
+        let currOldestDeath = currOldest.yearOfDeath ? currOldest.yearOfDeath : currYear; 
+        let personDeath = person.yearOfDeath ? person.yearOfDeath : currYear; 
+        return personDeath - person.yearOfBirth 
+        > currOldestDeath - currOldest.yearOfBirth ? person : currOldest;
 
+    }, {name: 'unborn', yearOfBirth: 1999, yearOfDeath: 1999})    
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
