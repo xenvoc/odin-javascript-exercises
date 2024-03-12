6a4979b7ea3c31a830465cc11a2d4a7fb4d6a3d5
Hectorinho <whatev@supposedEmail.net>
2024-03-12 16:22:28 +0200

    Do 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,29 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accumulator, val) => {
+    return accumulator + val;
+  }, 0)	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, value) => accumulator * value,1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num < 2) return 1;
+  else return num * factorial(num-1);
 	
 };
 
